9af30ca25372f20d52da9089272d1d1ed6ba6ede
Sam747-Ghost <samarthpatel281@hotmail.com>
2024-11-16 01:30:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const loginButton = document.getElementById('login-button');
+const mainContainer = document.getElementById('main-container');
+const loginContainer = document.getElementById('login-container');
+
+loginButton.addEventListener('click', () => {
+    const password = document.getElementById('password').value;
+    if (password === 'Archi0701') { // Change 'yourpassword' to your desired password
+        loginContainer.style.opacity = 0;
+        loginContainer.style.visibility = 'hidden';
+        mainContainer.style.display = 'flex';
+        setTimeout(() => {
+            mainContainer.classList.add('show-main');
+        }, 1000); // Match the CSS transition duration
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
+});
+
+window.addEventListener('load', () => {
+    mainContainer.classList.add('transition');
+});
